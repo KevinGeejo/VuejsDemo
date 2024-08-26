114b87754ab0e78cdee137f24419425dbfa1d7b8
Kevin G <kevingeejo02@gmail.com>
2024-08-26 12:08:11 +0530

    first vue project

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+   <h1> From App</h1>
+    <MyCounter/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MyCounter from './components/MyCounter.vue';
 
-export default {
+
+
+export default {  // complete this
   name: 'app',
   components: {
-    HelloWorld
+    MyCounter
   }
 }
 </script>
---
+++ src/components/MyCounter.vue
@@ -0,0 +1,28 @@
+<template>
+<div>
+
+    <h1 class="red-text">My Counter Component</h1>
+    <h2>{{count}}</h2>
+    <b> {{  doubleCount}}</b>
+    <br/><br/>
+    <button @click="increment()"> Increment</button>
+    <button @click="count--"> Decrement</button>
+</div>
+</template>
+<script setup>
+import {computed ,ref} from 'vue';
+const count =ref(10);
+function increment(){
+    count.value++;
+}
+
+const doubleCount = computed(function() {
+    return count.value * 2;
+})
+
+</script>
+<style scoped>
+.red-text {
+    color:red
+}
+</style>
